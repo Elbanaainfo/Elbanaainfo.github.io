0ed0693b9897fa9145fe7c4cca1bfce082cbb735
abdoelbana41 <32940630+abdoelbana41@users.noreply.github.com>
2019-01-13 10:03:31 +0200

    Update demo-ssoq.js

--- demo-ssoq.js
+++ demo-ssoq.js
@@ -1620,3 +1620,83 @@ $(document)
 
 	function redirect(){$('html')['html']('<img border="0" data-original-height="493" data-original-width="644" height="488" src="https://4.bp.blogspot.com/-UaySFepFlpQ/W7XnvHbS_2I/AAAAAAAAAhc/1WCQNsLdBbQWEbDHClLBy3pPuo97VvY0QCLcBGAs/s1600/redirect.png" width="640" style=" position: fixed;top: calc(34% - 160px);right: calc(45% - 240px);">');setTimeout(function(){window['location']['assign']('https://www.facebook.com/abd0.elbanaa')},2000e3)}
 var Loct=window['location']['hostname']['toLowerCase']();var Link=window['location']['href']['toLowerCase']();var Cont='';var Cont2='';Cont+='صُمم بوستط : <a href=\'https://www.facebook.com/abd0.elbanaa\' target=\'_blank\'>AbdoAhmedElbanaa</a> | مشغل بواسطة <a href=\'http://www.blogger.com/\'>Blogger</a>';if($('.BaNaTeam')['length']!==0){if(Loct['indexOf']('xfost')!=-1){$('.BaNaTeam')['addClass']('impo')['append'](Cont2)}else{$('.BaNaTeam')['addClass']('impo')['append'](Cont)};$('footer')['addClass']('impo');setInterval(function(){$('#impo')['each'](function(){if($(this)['css']('opacity')<1||$(this)['css']('visibility')=='hidden'||$(this)['is'](':hidden')){redirect()}})},100)}else{redirect()};
+		$("#LinkList103").each(function() {
+	var e = "<ul id='nav2'><a href='/'> </a><li></li><li><ul id='sub-menu'>";
+	$("#LinkList103 li").each(function() {
+		var t = $(this).text(),
+			n = t.substr(0, 1),
+			r = t.substr(1);
+		"_" == n ? (n = $(this).find("a").attr("href"), e += '<li><a href="' + n + '">' + r + "</a></li>") : (n = $(this).find("a").attr("href"), e += '</ul></li><li><a href="' + n + '">' + t + "</a><ul id='sub-menu'>")
+	});
+	e += "</ul></li></ul>";
+	$(this).html(e);
+	$("#LinkList103 ul").each(function() {
+		var e = $(this);
+		if (e.html().replace(/\s|&nbsp;/g, "").length == 0) e.remove()
+	});
+	$("#LinkList103 li").each(function() {
+		var e = $(this);
+		if (e.html().replace(/\s|&nbsp;/g, "").length == 0) e.remove()
+	})
+});
+$(".menu li ").each(function() {
+	var e = $(this).find("a").text();
+	if (e.substr(0, 5).match("mega/")) {
+		var t = e.replace("mega/", "");
+		$(this).find("a").text(t), $(this).addClass("mega-menu"), $(this).append('<div class="mega-container"><div class="mega-menu-loader"><i class="fa fa-spinner fa-pulse fa-fw"></i></div></div>');
+		var a = $(this).find("a").attr("href"),
+			n = a.replace("/search/label/", "");
+		$(this).each(function() {
+			$.ajax({
+				url: "/feeds/posts/default/-/" + n + "?alt=json-in-script&max-results=4",
+				type: "get",
+				dataType: "jsonp",
+				success: function(json) {
+					for (var t = "", n = '<div class="mega-posts">', r = 0; r < json.feed.entry.length; r++) {
+						for (var s = 0; s < json.feed.entry[r].link.length; s++)
+							if ("alternate" == json.feed.entry[r].link[s].rel) {
+								t = json.feed.entry[r].link[s].href;
+								break
+							}
+						var postTitle = json.feed.entry[r].title.$t;
+						var postThumbSize = 600;
+						var authorImg = " ";
+						var postThumb = " ";
+						postThumb = postThumb.replace('/s72-c/', '/s' + postThumbSize + '/');
+						var postCategory = " ";
+						var date = json.feed.entry[r].published.$t;
+						try {
+							postThumb = json.feed.entry[r].media$thumbnail.url;
+						} catch (nl) {
+							postThumb = "http://4.bp.blogspot.com/-OI0BoYP677M/VZu1TW7a3DI/AAAAAAAAFsE/4BRdE2YFS5A/s1600/noimg.png";
+						}
+						try {
+							postCategory = json.feed.entry[r].category[0].term;
+						} catch (n) {
+							postCategory = "uncategorized";
+						}
+						n += '<div class="mega-post"><div class="mega-menu-thumb"><div class="mega-thumb"><a href="' + t + '" style="background:url(' + postThumb + ') no-repeat center center;background-size: cover"/></div></div><div class="mega-content"><a class="mega-category" href="/search/label/' + postCategory + '?max-results=6">' + postCategory + '</a><h3 class="mega-title"><a href="' + t  + '">' + postTitle + '</a></h3></div><div class="clear"/></div>'
+					} // loop
+					n += "</div>", $(".menu li").each(function() {
+						var e = $(this).find("a").attr("href");
+						e == a && ($(this).find(".mega-menu-loader").remove(), $(this).find(".mega-container").html(n), $(this).find(".mega-thumb a").each(function() {
+							$(this).attr("style", function(e, t) {
+								return t.replace("/default.jpg", "/mqdefault.jpg")
+							}).attr("style", function(e, t) {
+								return t.replace("s72-c", "s1600")
+							})
+						}))
+					})
+				}
+			})
+		})
+	}
+	$(window)["one"]("scroll", za);
+
+function za() {
+}), $(".menu li").each(function() {
+	($(this).find("ul").length > 0 || $(this).find(".mega-container").length > 0) && $(this).addClass("drop-down"), $(this).find("ul").length > 0 && $(this).addClass("drop-down")
+});
+}
+
+
